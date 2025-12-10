b2978d6d32261b121a692874f3d87b25251a039c
Vivit Kawamura <vivitkawamura@gmail.com>
2025-12-11 08:43:29 +0900

    Save current state for final merge

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MuiIndex from './page/mui/index.tsx';
 
 // GitHub Pages にデプロイする場合、basename を設定
+// GitHubのリポジトリで Rulesets（ルールセット）を作成
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
