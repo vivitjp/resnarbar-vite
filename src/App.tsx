2e0375388464386a50a0072ea11a7bc46e427631
Vivit Kawamura <vivitkawamura@gmail.com>
2025-12-11 18:00:25 +0900

    udpate

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { Link } from 'react-router-dom'
 import './App.css'
+import { Typography } from '@mui/material'
 
 function App() {
   return (
     <>
       <div className="card">
-        <p>
+        <Typography variant="h4" component="h1" gutterBottom>
           お品書き
-        </p>
+        </Typography>
         <Link to="/mui" style={{ display: 'inline-block', marginTop: 16, color: '#1976d2', textDecoration: 'underline' }}>
           MUI Tooltip サンプル集
         </Link>
